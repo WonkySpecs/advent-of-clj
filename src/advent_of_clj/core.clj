09854752f8630ebe709bee6aa871de2547ec1c25
Will Taylor <w1ll100@hotmail.co.uk>
2018-12-04 23:51:43 +0000

    Day 2 Progress

--- src/advent_of_clj/core.clj
+++ src/advent_of_clj/core.clj
@@ -21,10 +21,39 @@
 				new_freq
 				(recur change_vec new_freq (drop 1 changes) (conj seen_freqs new_freq))))))
 
+
+(defn count_letters_in_string
+	([s counts]
+		;If string empty, return counts
+		(if (= (count s) 0)
+			counts
+			;Otherwise, if first letter is a key in 'counts', increment it's value; otherwise add it value 1
+			(do (let [c (subs s 0 1)]
+				(let [c_count (get counts c)]
+				(count_letters_in_string (subs s 1 (count s))
+										 (if c_count
+										 	(update counts c inc)
+										 	(conj counts [c 1]))))))))
+	([s]
+		(count_letters_in_string s {})))
+
+(defn has_value [letter_count n]
+	(doseq [kv letter_count]
+		(= (second kv) n)))
+	
+
+(defn calc_checksum [input]
+	(loop [[string & remaining] input twos 0 threes 0]
+		(if (nil? string)
+			(* twos threes)
+			(let [letter_count (count_letters_in_string string)]
+				(recur remaining (+ (has_value letter_count 2) twos) (+ (has_value letter_count 3) threes))))))
+
 (defn -main
   [& args]
   (def day (subs (first args) 0 1))
   (def input (get_input_vector day))
   (println (case (first args)
 	  "1a" (reduce + input)
-	  "1b" (find_first_reccuring_freq input 0 (apply list input) #{}))))
+	  "1b" (find_first_reccuring_freq input 0 (apply list input) #{})
+	  "2a" (calc_checksum input))))
